faec1b69f37aa5c46b84d966052435b520c9690f
Anshu <raianshu8991@gmail.com>
2020-01-31 13:39:00 -0500

    basic setup

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View,
+  StatusBar
 
+
+} from 'react-native';
+import Button from './components/button';
+import TextCustom from './components/text';
+import Gradient from 'react-native-css-gradient';
+ 
 export default function App() {
+
+  const gradient = `linear-gradient(-225deg, #231557 0%, #44107A 29%, #FF1361 67%, #FFF800 100%),
+  repeating-linear-gradient(-115deg, transparent, transparent 20px, rgba(255,255,255,0.1) 20px, rgba(255,255,255,0.1) 40px),
+  repeating-linear-gradient(115deg, transparent, transparent 20px, rgba(255,255,255,0.1) 20px, rgba(255,255,255,0.1) 40px)`;
+
   return (
+
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+   
+  <StatusBar backgroundColor="blue" barStyle="light-content" />
+  <Gradient gradient={gradient} />
+
+ 
+
+    <Text style={styles.textstyle}> Welcome To Colossal</Text>
+    <View style={
+      {
+        borderBottomColor:"blue",
+        borderBottomWidth:5,
+        borderRadius:10
+  
+  
+  }}/>
+   
+     
     </View>
   );
 }
@@ -14,6 +47,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+
+  textstyle:
+  {
+    fontSize:35,
+    marginTop:35,
+    textAlign:'center',
+    color:'#0000ff',
+    fontWeight:'800'
+
+  }
 });
---
+++ components/text.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Text,StyleSheet} from 'react-native-paper';
+
+
+
+const TextComponent=()=>
+{
+       <Text style={styles.container}>Welcome To Colossal</Text>
+}
+
+
+
+
+export default TextComponent;
