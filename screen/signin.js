6a1f00076fa63745735ff7b57efb4f676b1d78da
Anshu <raianshu8991@gmail.com>
2020-02-05 12:55:39 -0500

    signin

--- screen/signin.js
+++ screen/signin.js
@@ -4,33 +4,65 @@ import {
   Text,
   View,
   StatusBar,
-  Header
+  Header,
+
 
 
 } from 'react-native';
-import Button from '../components/button';
-import TextCustom from '../components/text';
-import { LinearGradient } from 'expo-linear-gradient';
-import Myheader from '../components/myheader';
-import Lgradient from '../components/layout';
- 
-export default function App() {
+import Button from './components/button';
+import TextCustom from './components/text';
+import LGradient from './components/layout';
+import Myheader from './components/myheader';
+import {createAppContainer}  from 'react-navigation';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {TextInput} from 'react-native-paper';
+import {Ionicons}  from '@expo/vector-icons';
 
-  
+
+
+
+
+
+
+
+const Signin=()=>
+{
+
+ 
+
 
   return (
 
-   
+    
     <View style={styles.container}>
       <Myheader />
      <StatusBar backgroundColor="blue" barStyle="light-content" />
-     
+   
   
+   <LGradient />
+   
+ 
 
-    <Lgradien />
+  <View style={{backgroundColor:'#2187C4',paddingTop:20,paddingBottom:40 ,paddingRight:50,paddingLeft:50,marginTop:100} }> 
+   <Text style={styles.textstyle}>Signin</Text>
+   
 
-    <Text style={styles.textstyle}> SignIn</Text>
-
+   
+    <TextInput  
+   style={styles.textinputstyle}
+      placeholder='Email'
+    
+    />
+    <TextInput  
+    style={styles.textinputstyle}
+      placeholder='Password'
+    
+    />
+   
+    
+    <Button  mode='contained'/>
+  
+    </View>
      
     </View>
   );
@@ -52,6 +84,22 @@ const styles = StyleSheet.create({
     fontWeight:'800'
 
   },
+  icon:
+  {
+    marginBottom:-45,
+    zIndex:2,
+    marginLeft:2,
+    padding:3
+  },
+  textinputstyle:
+  {
+    height:30 ,
+    width:200,
+    padding:6,
+    borderRadius: 4,
+    marginTop:10,
+    textAlign:'center'
+  }
 
 
 });
